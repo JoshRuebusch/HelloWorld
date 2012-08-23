a55a4cbd2f353be989019405f7feec45d23b7ff2
Joshua Ruebusch <ruebusjw@muohio.edu>
2012-08-23 10:19:37 -0400

    I wanted it red

--- src/HelloWorldApp.cpp
+++ src/HelloWorldApp.cpp
@@ -29,7 +29,7 @@ void HelloWorldApp::update()
 void HelloWorldApp::draw()
 {
 	// clear out the window with black
-	gl::clear( Color( 0, 0, 0 ) ); 
+	gl::clear( Color( 255, 0, 0 ) ); 
 }
 
 CINDER_APP_BASIC( HelloWorldApp, RendererGl )
